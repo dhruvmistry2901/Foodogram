b850d83c67d3038963be3fc81ea7c8e08fb8eb46
mulesiddhi <mulesiddhi@gmail.com>
2022-04-20 13:03:25 +0530

    js

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 //Sidebar
 const menuItems=document.querySelectorAll('.menu-item');
+
+const changeActiveItem=()=>{
+    menuItems.forEach(item=>{
+        item.classList.remove('active');
+    });
+}
+
+menuItems.forEach(item=>{
+    item.addEventListener('click',()=>{
+        changeActiveItem();
+        item.classList.add('active');
+      
+    });
+})
+
+//messages
+const message=document.querySelector('.messages');
+const messageNotification=document.querySelector('#message-notification');
+
+messageNotification.addEventListener('click',()=>{
+    message.style.boxShadow='0 0 1rem var(--color-primary)';
+    setTimeout(()=>{
+        message.style.boxShadow='none';
+    },2000);
+});
+
